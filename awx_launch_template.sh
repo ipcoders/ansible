58959ad2c026fa01daaa9e35fb3df8ef07533bbc
IPcoders <78664838+ipcoders@users.noreply.github.com>
2024-03-04 15:10:32 -0500

    Create awx_launch_template.sh

---
+++ awx_launch_template.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Variables
+AWX_HOST="54.221.180.223:8043"
+JOB_TEMPLATE_ID="7"
+LAUNCH_ENDPOINT="/api/v2/job_templates/${JOB_TEMPLATE_ID}/launch/"
+ADMIN_TOKEN="wGsqreQ2LH6413uK1FzYht96q7Bh0L"
+CONTENT_TYPE="Content-Type: application/json"
+AUTHORIZATION="Authorization: Bearer ${ADMIN_TOKEN}"
+CHECK_INTERVAL=5 # How often to check the job status, in seconds
+
+# Launch the job template
+response=$(curl -s -k -X POST \
+                  -H "${CONTENT_TYPE}" \
+                  -H "${AUTHORIZATION}" \
+                  -d "{}" \
+                  "https://${AWX_HOST}${LAUNCH_ENDPOINT}")
+
+# Extract job ID and stdout URL from the response
+job_id=$(echo "${response}" | jq -r '.job')
+stdout_url=$(echo "${response}" | jq -r '.related.stdout')
+
+echo "Job ID: $job_id"
+echo "Stdout URL: $stdout_url"
+
+# Function to check job status
+function check_job_status {
+    curl -s -k -H "${AUTHORIZATION}" "https://${AWX_HOST}/api/v2/jobs/${job_id}/" | jq -r '.status'
+}
+
+# Poll the job status
+status=$(check_job_status)
+while [[ "$status" != "successful" && "$status" != "failed" ]]; do
+    echo "Current job status: $status"
+    sleep $CHECK_INTERVAL
+    status=$(check_job_status)
+done
+
+echo "Job completed with status: $status"
+
+# Fetch and display the job result if the job was successful
+if [ "$status" = "successful" ]; then
+    echo "Fetching job output..."
+    curl -s -k -H "${AUTHORIZATION}" "https://${AWX_HOST}${stdout_url}?format=txt" > results.txt 
+else
+    echo "Job failed. Stdout not fetched."
+fi
